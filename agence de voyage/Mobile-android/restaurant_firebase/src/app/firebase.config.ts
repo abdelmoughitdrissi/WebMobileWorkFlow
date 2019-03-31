2a658b8fe9aba22f46dd81438983fafe478f681c
abdelmoughitdrissi <40865579+abdelmoughitdrissi@users.noreply.github.com>
2019-03-31 18:11:34 +0100

    abdelmoughitdrissi
    Rahul +

---
+++ agence de voyage/Mobile-android/restaurant_firebase/src/app/firebase.config.ts
@@ -0,0 +1,21 @@
+//config default
+export const firebaseConfig = {
+  apiKey: "AIzaSyAmHvzwv4999iu2Z-ucvzWEQtWVbpniQIU",
+    authDomain: "skyjet-59885.firebaseapp.com",
+    databaseURL: "https://skyjet-59885.firebaseio.com",
+    projectId: "skyjet-59885",
+    storageBucket: "skyjet-59885.appspot.com",
+    messagingSenderId: "613467106093"
+};
+
+// Sandip's firebase configuration
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyAzusy10a0pSk-huxJO-DUtK0RoGgqJcpM",
+//     authDomain: "restaurantweb-ionic-app.firebaseapp.com",
+//     databaseURL: "https://restaurantweb-ionic-app.firebaseio.com",
+//     projectId: "restaurantweb-ionic-app",
+//     storageBucket: "restaurantweb-ionic-app.appspot.com",
+//     messagingSenderId: "120142074708"
+//   };
+
+
