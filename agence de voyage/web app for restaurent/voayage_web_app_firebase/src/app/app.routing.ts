7c72ff9b6b85371a0498de0f02eed8d68da880d5
abdelmoughitdrissi <40865579+abdelmoughitdrissi@users.noreply.github.com>
2019-03-31 19:11:55 +0100

    Revert "abdelmoughitdrissi"
    This reverts commit 2a658b8fe9aba22f46dd81438983fafe478f681c.

--- agence de voyage/web app for restaurent/voayage_web_app_firebase/src/app/app.routing.ts
+++
@@ -1,7 +0,0 @@
-import {Routes, RouterModule} from'@angular/router';
-
-const APP_ROUTES:Routes=[
-
-  { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
-];
-export const Routing = RouterModule.forRoot(APP_ROUTES);
--- agence de voyage/web app for restaurent/voayage_web_app_firebase/src/app/app.ts
+++
@@ -1,8 +0,0 @@
-export interface cart {
-	productId: string,
-	name: string,
-	price: number,
-	Quantity: number,
-	itemTotalPrice: number,
-	imageUrl: string
-}
